376304a7be47ba8726461c17933e3b297890c680
asrorbek07 <asrorobidjonov2000@gmail.com>
2021-07-09 15:22:35 +0500

    meet section finished

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ if (elTogglerBtn) {
     elTogglerBtn.addEventListener('click', function(evt){
         elSiteHeader.classList.toggle('site-header__open')
     })
-    
+}
+
+let elMeetBtn = document.querySelectorAll('.meet__btn');
+if (elMeetBtn.length>0) {
+elMeetBtn.forEach( function(btn){
+ btn.addEventListener('click', function(evt){
+     evt.preventDefault();
+     btn.parentElement.classList.toggle('meet__open-toggler')
+     console.log('hf')
+ })
+})
 }
